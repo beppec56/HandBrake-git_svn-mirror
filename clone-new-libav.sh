0a80eb536386f6f17fe8812c41c4cff6c1682bd2
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2014-01-24 19:04:11 +0100

    bump libav, libav-v10_beta1-1-gf87ce26-local
    Conflicts:
    
    	contrib/ffmpeg/module.defs

---
+++ clone-new-libav.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#
+cd download
+#if [ ! -d libav ]; then
+#   git clone git://git.libav.org/libav.git
+#fi
+
+cd libav
+
+git checkout master
+git  pull origin
+if [ "$1" != "" ]; then
+    git checkout $1
+else # use current default
+    git checkout master
+fi
+
+GITVERS=$(./version.sh)
+SOURCE_DIR=$(echo "libav-$GITVERS""-local")
+TARFILE=`echo "$SOURCE_DIR"".tar.bz2"`
+
+
+cd ..
+
+rm -rf "$SOURCE_DIR"
+mkdir -p  "$SOURCE_DIR"
+cp -Ppruvf libav/* "$SOURCE_DIR"
+rm -rf  "$SOURCE_DIR"".git"
+
+tar -hcjf $TARFILE $SOURCE_DIR
+
+rm -rf $SOURCE_DIR
