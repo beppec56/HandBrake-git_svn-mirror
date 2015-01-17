beppec56/HandBrake-git_svn-mirror

commit ace058f5eb7a8e67f99b6819493602e3518a2bb9

Simplify frontend useage of hb_add() Modify hb_add() to automatically add all necessary passes, so hb_add() only needs to be called once per job.  It now automatically adds subtitle scan and 2-pass encoding passes.
git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@6738 b64f7644-9d1e-0410-96f1-a4d463321fa5

libhb/hb.c
@@ -1255,7 +1255,7 @@ hb_job_t * hb_current_job( hb_handle_t * h )
  * @param h Handle to hb_handle_t.
  * @param job Handle to hb_job_t.
  */
-void hb_add( hb_handle_t * h, hb_job_t * job )
+static void hb_add_internal( hb_handle_t * h, hb_job_t * job )
 {
     hb_job_t      * job_copy;
     hb_audio_t    * audio;
@@ -1345,6 +1345,40 @@ void hb_add( hb_handle_t * h, hb_job_t * job )
     hb_list_add( h->jobs, job_copy );
     h->job_count = hb_count(h);
     h->job_count_permanent++;
+}
+
+void hb_add( hb_handle_t * h, hb_job_t * job )
+{
+    int sub_id = 0;
+
+    if (job->vquality >= 0)
+    {
+        job->twopass = 0;
+    }
+    if (job->indepth_scan)
+    {
+        hb_deep_log(2, "Adding subtitle scan pass");
+        job->pass = -1;
+        job->sequence_id = (job->sequence_id & 0xFFFFFF) | (sub_id++ << 24);
+        hb_add_internal(h, job);
+        job->indepth_scan = 0;
+    }
+    if (job->twopass)
+    {
+        hb_deep_log(2, "Adding two-pass encode");
+        job->pass = 1;
+        job->sequence_id = (job->sequence_id & 0xFFFFFF) | (sub_id++ << 24);
+        hb_add_internal(h, job);
+        job->pass = 2;
+        job->sequence_id = (job->sequence_id & 0xFFFFFF) | (sub_id++ << 24);
+        hb_add_internal(h, job);
+    }
+    else
+    {
+        job->pass = 0;
+        job->sequence_id = (job->sequence_id & 0xFFFFFF) | (sub_id++ << 24);
+        hb_add_internal(h, job);
+    }
 }
 
 /**
@@ -1789,7 +1823,7 @@ void hb_set_state( hb_handle_t * h, hb_state_t * s )
 
         // Set which job is being worked on
         if (h->current_job)
-            h->state.param.working.sequence_id = h->current_job->sequence_id;
+            h->state.param.working.sequence_id = h->current_job->sequence_id & 0xFFFFFF;
         else
             h->state.param.working.sequence_id = 0;
     }

gtk/src/hb-backend.c
@@ -4178,7 +4178,6 @@ add_job(hb_handle_t *h, GValue *js, gint unique_id, int titleindex)
     hb_list_t  * list;
     const hb_title_t * title;
     hb_job_t   * job;
-    gint sub_id = 0;
     hb_filter_object_t * filter;
     gchar *filter_str;
     gchar *dest_str = NULL;
@@ -4705,72 +4704,11 @@ add_job(hb_handle_t *h, GValue *js, gint unique_id, int titleindex)
     }
     free(meta);
 
-    if (job->indepth_scan == 1)
-    {
-        // Subtitle scan. Look for subtitle matching audio language
-
-        /*
-         * When subtitle scan is enabled do a fast pre-scan job
-         * which will determine which subtitles to enable, if any.
-         */
-        job->pass = -1;
-        job->indepth_scan = 1;
-        hb_job_set_encoder_options(job, NULL);
-
-        /*
-         * Add the pre-scan job
-         */
-        job->sequence_id = (unique_id & 0xFFFFFF) | (sub_id++ << 24);
-        hb_add( h, job );
-    }
-
-    if( ghb_settings_get_boolean(js, "VideoTwoPass") &&
-        !ghb_settings_get_boolean(js, "vquality_type_constant"))
-    {
-        /*
-         * If subtitle_scan is enabled then only turn it on
-         * for the second pass and then off again for the
-         * second.
-         */
-        job->pass = 1;
-        job->indepth_scan = 0;
-        ghb_set_video_encoder_opts(job, js);
-
-        /*
-         * If turbo options have been selected then set job->fastfirstpass
-         */
-        if(ghb_settings_get_boolean(js, "VideoTurboTwoPass") &&
-           (job->vcodec == HB_VCODEC_X264 || job->vcodec == HB_VCODEC_X265))
-        {
-            job->fastfirstpass = 1;
-        }
-        else
-        {
-            job->fastfirstpass = 0;
-        }
-
-        job->sequence_id = (unique_id & 0xFFFFFF) | (sub_id++ << 24);
-        hb_add( h, job );
-
-        job->pass = 2;
-        /*
-         * On the second pass we turn off subtitle scan so that we
-         * can actually encode using any subtitles that were auto
-         * selected in the first pass (using the whacky select-subtitle
-         * attribute of the job).
-         */
-        job->indepth_scan = 0;
-        job->sequence_id = (unique_id & 0xFFFFFF) | (sub_id++ << 24);
-        hb_add( h, job );
-    }
-    else
-    {
-        ghb_set_video_encoder_opts(job, js);
-        job->indepth_scan = 0;
-        job->pass = 0;
-        job->sequence_id = (unique_id & 0xFFFFFF) | (sub_id++ << 24);
-        hb_add( h, job );
-    }
+    job->twopass = ghb_settings_get_boolean(js, "VideoTwoPass");
+    job->fastfirstpass = ghb_settings_get_boolean(js, "VideoTurboTwoPass");
+    job->sequence_id = unique_id;
+    ghb_set_video_encoder_opts(job, js);
+    hb_add(h, job);
 
     hb_job_close(&job);
 }

libhb/common.c
@@ -3001,6 +3001,7 @@ static void job_setup(hb_job_t * job, hb_title_t * title)
     job->vcodec     = HB_VCODEC_FFMPEG_MPEG4;
     job->vquality   = -1.0;
     job->vbitrate   = 1000;
+    job->twopass    = 0;
     job->pass       = 0;
     job->vrate      = title->vrate;
 

libhb/common.h
@@ -494,7 +494,8 @@ struct hb_job_s
     int             vbitrate;
     hb_rational_t   vrate;
     int             cfr;
-    int             pass;
+    PRIVATE int     pass;
+    int             twopass;        // Enable 2-pass encode. Boolean
     int             fastfirstpass;
     char           *encoder_preset;
     char           *encoder_tune;

libhb/hb_json.c
@@ -479,7 +479,7 @@ char* hb_job_to_json( const hb_job_t * job )
     else
     {
         json_object_set_new(video_dict, "Bitrate", json_integer(job->vbitrate));
-        json_object_set_new(video_dict, "TwoPass", json_boolean(job->pass));
+        json_object_set_new(video_dict, "TwoPass", json_boolean(job->twopass));
         json_object_set_new(video_dict, "Turbo",
                             json_boolean(job->fastfirstpass));
     }
@@ -705,6 +705,7 @@ hb_job_t* hb_json_to_job( hb_handle_t * h, const char * json_job )
 
     result = json_unpack_ex(dict, &error, 0,
     "{"
+    // SequenceID
     "s:i,"
     // Destination {File, Mux, ChapterMarkers, Mp4Options {
     //                          Mp4Optimize, IpodAtom}
@@ -758,7 +759,7 @@ hb_job_t* hb_json_to_job( hb_handle_t * h, const char * json_job )
             "Profile",              unpack_s(&video_profile),
             "Level",                unpack_s(&video_level),
             "Options",              unpack_s(&video_options),
-            "TwoPass",              unpack_b(&job->pass),
+            "TwoPass",              unpack_b(&job->twopass),
             "Turbo",                unpack_b(&job->fastfirstpass),
             "ColorMatrixCode",      unpack_i(&job->color_matrix_code),
         "Audio",
@@ -1087,27 +1088,7 @@ int hb_add_json( hb_handle_t * h, const char * json_job )
     if (job == NULL)
         return -1;
 
-    if (job->indepth_scan)
-    {
-        hb_deep_log(2, "Adding subtitle scan pass");
-        int pass = job->pass;
-        job->pass = -1;
-        hb_add(h, job);
-        job->pass = pass;
-        job->indepth_scan = 0;
-    }
-    if (job->pass)
-    {
-        hb_deep_log(2, "Adding two-pass encode");
-        job->pass = 1;
-        hb_add(h, job);
-        job->pass = 2;
-        hb_add(h, job);
-    }
-    else
-    {
-        hb_add(h, job);
-    }
+    hb_add(h, job);
     hb_job_close(&job);
 
     return 0;

macosx/HBCore.m
@@ -238,54 +238,7 @@ NSString *HBCoreMuxingNotification = @"HBCoreMuxingNotification";
     [HBUtilities writeToActivityLog: "processNewQueueEncode number of passes expected is: %d", (job.video.twoPass + 1)];
     hb_job_set_file(hb_job, job.destURL.path.fileSystemRepresentation);
 
-    // If scanning we need to do some extra setup of the job.
-    if (hb_job->indepth_scan == 1)
-    {
-        char *encoder_preset_tmp  = hb_job->encoder_preset  != NULL ? strdup(hb_job->encoder_preset)  : NULL;
-        char *encoder_tune_tmp    = hb_job->encoder_tune    != NULL ? strdup(hb_job->encoder_tune)    : NULL;
-        char *encoder_options_tmp = hb_job->encoder_options != NULL ? strdup(hb_job->encoder_options) : NULL;
-        char *encoder_profile_tmp = hb_job->encoder_profile != NULL ? strdup(hb_job->encoder_profile) : NULL;
-        char *encoder_level_tmp   = hb_job->encoder_level   != NULL ? strdup(hb_job->encoder_level)   : NULL;
-        /*
-         * When subtitle scan is enabled do a fast pre-scan job
-         * which will determine which subtitles to enable, if any.
-         */
-        hb_job_set_encoder_preset (hb_job, NULL);
-        hb_job_set_encoder_tune   (hb_job, NULL);
-        hb_job_set_encoder_options(hb_job, NULL);
-        hb_job_set_encoder_profile(hb_job, NULL);
-        hb_job_set_encoder_level  (hb_job, NULL);
-        hb_job->pass = -1;
-        hb_add(self.hb_handle, hb_job);
-        /*
-         * reset the advanced settings
-         */
-        hb_job_set_encoder_preset (hb_job, encoder_preset_tmp);
-        hb_job_set_encoder_tune   (hb_job, encoder_tune_tmp);
-        hb_job_set_encoder_options(hb_job, encoder_options_tmp);
-        hb_job_set_encoder_profile(hb_job, encoder_profile_tmp);
-        hb_job_set_encoder_level  (hb_job, encoder_level_tmp);
-        free(encoder_preset_tmp);
-        free(encoder_tune_tmp);
-        free(encoder_options_tmp);
-        free(encoder_profile_tmp);
-        free(encoder_level_tmp);
-    }
-
-    if (job.video.twoPass)
-    {
-        hb_job->indepth_scan = 0;
-        hb_job->pass = 1;
-        hb_add(self.hb_handle, hb_job);
-        hb_job->pass = 2;
-        hb_add(self.hb_handle, hb_job);
-    }
-    else
-    {
-        hb_job->indepth_scan = 0;
-        hb_job->pass = 0;
-        hb_add(self.hb_handle, hb_job);
-    }
+    hb_add(self.hb_handle, hb_job);
 
     // Free the job
     hb_job_close(&hb_job);

macosx/HBJob.m
@@ -233,6 +233,7 @@ NSString *keyContainerTag                      = @"keyContainerTag";
         job->ipod_atom = self.mp4iPodCompatible;
     }
 
+    job->twopass = self.video.twoPass;
     if (job->vcodec == HB_VCODEC_X264 || job->vcodec == HB_VCODEC_X265)
     {
         // set fastfirstpass if 2-pass and Turbo are enabled

macosx/HBPreviewGenerator.m
@@ -242,46 +242,8 @@ typedef enum EncodeState : NSUInteger {
     self.core = [[[HBCore alloc] initWithLoggingLevel:loggingLevel] autorelease];
     self.core.name = @"PreviewCore";
 
-    /*
-     * If scanning we need to do some extra setup of the job.
-     */
-    if (job->indepth_scan == 1)
-    {
-        char *encoder_preset_tmp  = job->encoder_preset  != NULL ? strdup(job->encoder_preset)  : NULL;
-        char *encoder_tune_tmp    = job->encoder_tune    != NULL ? strdup(job->encoder_tune)    : NULL;
-        char *encoder_options_tmp = job->encoder_options != NULL ? strdup(job->encoder_options) : NULL;
-        char *encoder_profile_tmp = job->encoder_profile != NULL ? strdup(job->encoder_profile) : NULL;
-        char *encoder_level_tmp   = job->encoder_level   != NULL ? strdup(job->encoder_level)   : NULL;
-        /*
-         * When subtitle scan is enabled do a fast pre-scan job
-         * which will determine which subtitles to enable, if any.
-         */
-        hb_job_set_encoder_preset (job, NULL);
-        hb_job_set_encoder_tune   (job, NULL);
-        hb_job_set_encoder_options(job, NULL);
-        hb_job_set_encoder_profile(job, NULL);
-        hb_job_set_encoder_level  (job, NULL);
-        job->pass = -1;
-        hb_add(self.core.hb_handle, job);
-        /*
-         * reset the advanced settings
-         */
-        hb_job_set_encoder_preset (job, encoder_preset_tmp);
-        hb_job_set_encoder_tune   (job, encoder_tune_tmp);
-        hb_job_set_encoder_options(job, encoder_options_tmp);
-        hb_job_set_encoder_profile(job, encoder_profile_tmp);
-        hb_job_set_encoder_level  (job, encoder_level_tmp);
-        free(encoder_preset_tmp);
-        free(encoder_tune_tmp);
-        free(encoder_options_tmp);
-        free(encoder_profile_tmp);
-        free(encoder_level_tmp);
-    }
 
     /* Go ahead and perform the actual encoding preview scan */
-    job->indepth_scan = 0;
-    job->pass = 0;
-
     hb_add(self.core.hb_handle, job);
 
     /* we need to clean up the various lists after the job(s) have been set  */

commit 6b2844e04f8add5d0df97065449141debe21da0c

[libhb] Force fastfirst pass, to temporary fix a problem introduced in r6738, git-ace058f. REMOVE WHEN FIXED UPSTREAM

libhb/hb.c
@@ -1368,9 +1368,11 @@ void hb_add( hb_handle_t * h, hb_job_t * job )
         hb_deep_log(2, "Adding two-pass encode");
         job->pass = 1;
         job->sequence_id = (job->sequence_id & 0xFFFFFF) | (sub_id++ << 24);
+	job->fastfirstpass = 1;	
         hb_add_internal(h, job);
         job->pass = 2;
         job->sequence_id = (job->sequence_id & 0xFFFFFF) | (sub_id++ << 24);
+	job->fastfirstpass = 0;	
         hb_add_internal(h, job);
     }
     else